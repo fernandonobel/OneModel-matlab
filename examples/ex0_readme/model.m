86286fe66e4b4b9b8b6df8cdaf6a1aaf1ab47cef
FernandoNobel <fersann1@upv.es>
2020-07-27 18:45:50 +0200

    updated the readme

--- examples/ex0_readme/model.m
+++ examples/ex0_readme/model.m
@@ -13,37 +13,37 @@ classdef model < ModelClass
 			v = VariableClass('ref');
 			v.isSubstitution=true;
 			e = EquationClass('');
-			e.eqn = 'ref==k3/d3';
-			e.isSubstitution=true;
+			e.eqn = 'ref ==  k3/d3';
+			e.isSubstitution = true;
 			obj.addEquation(e);
 			obj.addVariable(v);
 
 			p = ParameterClass('k1');
-			p.value=1.0;
+			p.value = 1.0;
 			obj.addParameter(p);
 
 			p = ParameterClass('k2');
-			p.value=1.0;
+			p.value = 1.0;
 			obj.addParameter(p);
 
 			p = ParameterClass('k3');
-			p.value=1.0;
+			p.value = 1.0;
 			obj.addParameter(p);
 
 			p = ParameterClass('d1');
-			p.value=1.0;
+			p.value = 1.0;
 			obj.addParameter(p);
 
 			p = ParameterClass('d2');
-			p.value=1.0;
+			p.value = 1.0;
 			obj.addParameter(p);
 
 			p = ParameterClass('d3');
-			p.value=1.0;
+			p.value = 1.0;
 			obj.addParameter(p);
 
 			p = ParameterClass('gamma12');
-			p.value=1.0;
+			p.value = 1.0;
 			obj.addParameter(p);
 
 			e = EquationClass('');
@@ -58,6 +58,7 @@ classdef model < ModelClass
 			e.eqn = 'der_x3 == k3*x1 - d3*x3';
 			obj.addEquation(e);
 
+		obj.checkValidModel();
 		end
 	end
 end
