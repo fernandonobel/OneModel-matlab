50d999d96a37a4bf84b8802054a36bd3a28a10e0
FernandoNobel <fersann1@upv.es>
2020-12-16 12:38:45 +0100

    Removed old command code from the parser

--- examples/ex5_class/ProteinClass.m
+++ examples/ex5_class/ProteinClass.m
@@ -2,3 +2,26 @@ classdef ProteinClass < ModelClass
 	methods
 		function [obj] = ProteinClass()
 
+			obj.simOptions.AbsTol = 1e-6;
+			% This is a test comment.
+
+			v = VariableClass('x1');
+			v.start = 0.0;
+			obj.addVariable(v);
+
+			p = ParameterClass('A');
+			p.value = 1;
+			obj.addParameter(p);
+
+			e = EquationClass('der_x1 == A');
+			e.eqn = '';
+			obj.addEquation(e);
+
+
+disp(1);
+
+
+			obj.checkValidModel();
+		end
+	end
+end
